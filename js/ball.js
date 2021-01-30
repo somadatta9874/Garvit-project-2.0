31098c85b8ab8a6f0b64b6811971c8ee01bf01d9
somadatta9874 <66265341+somadatta9874@users.noreply.github.com>
2021-01-30 20:01:50 +0530

    Add files via upload

---
+++ js/ball.js
@@ -0,0 +1,26 @@
+class Ball{
+    constructor(x,y,r){
+        var options ={
+            isStatic:false
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        this.r = r;
+        this.x = x;
+        this.y = y;
+        
+
+        this.image = loadImage("bowlingBall1.png");
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+       
+        var angle = this.body.angle;
+        push();
+        translate (pos.x,pos.y);
+        rotate(angle);
+        imageMode (CENTER);
+        image(this.image,0,0,this.r,this.r);
+        pop ();      
+    }
+}
